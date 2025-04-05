3bc4cd97bca28fa793f8149d4452ac5255570db2
ollieroe <114855485+ollieroe@users.noreply.github.com>
2025-04-05 12:11:11 +0100

    Updated homepage

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -12,35 +12,35 @@ function Footer () {
         <div className='footer'>
             <div className='footer-nav'>
 
-            <div>
+            <div className='footer-nav-column'>
+                <Link to="/" >
+                    <img src={logo} alt="Red Squirrel Logo" className='squirrel-footer' ></img>
+                </Link>
                 <h5>Heart of Down Red Squirrel Group</h5>
-                <Link to="/" className="navLeft">
-            <img src={logo} alt="Red Squirrel Logo" className="navLogo"></img>
-        </Link>
             </div>
-            <div>
+            <div className='footer-nav-column'>
                 <h5>Browse</h5>
-                        <Link to="/" className="navElement">Home</Link>
-                        <Link to="/about" className="navElement">About</Link>
-                        <Link to="/getInvolved" className="navElement">Get Involved</Link>
-                        <Link to="/sightings" className="navElement">Sightings</Link>
-            </div>
+                    <Link to="/" className="footer-link">Home</Link>
+                    <Link to="/about" className="footer-link">About</Link>
+                    <Link to="/getInvolved" className="footer-link">Get Involved</Link>
+                    <Link to="/sightings" className="footer-link">Sightings</Link>
+                </div>
 
-            <div>
+            <div className='footer-nav-column'>
                 <h5>Contact</h5>
-                <p>countydownredsquirrel@gmail.com</p>
-                <p>077894326</p>
+                <p>heartofdownredsquirrel@gmail.co.uk</p>
+                <p>07789432643</p>
                 <p>Castle Ward, Downpatrick, Northern Ireland</p>
             </div>
 
-            <div>
+            <div className='footer-nav-column'>
                 <h5>Get Involved</h5>
             </div>
             </div>
 
             <div className='logo-div'>
-                <div class="footer-logo" id="footer-1"></div>
-                <div class="footer-logo" id="footer-2"></div>
+                <div className="footer-logo" id="footer-1"></div>
+                <div className="footer-logo" id="footer-2"></div>
             </div>
 
             <div className='logo-div' id="bottom-footer">
--- src/pages/home.js
+++ src/pages/home.js
@@ -12,11 +12,18 @@ function Home () {
         <div className='main'>
             <div className='img-div'>
                 <img src={banner} alt='Image of a squirrel sat on a branch' id="sq-banner"></img>
+                <div className='hero-text'>
+                    <h1>We're the Heart of Down Red Squirrel Group</h1>
+                </div>
             </div>
-                <p className='info-text'>At The Heart of Down Red Squirrel Charity, we are dedicated to protecting and conserving one of the UK's most iconic and endangered species – the red squirrel. Once widespread across the country, red squirrels are now under threat due to habitat loss, disease, and competition from the invasive grey squirrel. </p>
-                    
-                <p className='info-text'> With fewer than 120,000 red squirrels left in the wild, their future is uncertain, but together, we can make a difference. Our charity focuses on vital conservation efforts such as habitat restoration, the installation of squirrel boxes, and monitoring squirrel populations to ensure their survival. Through education and community involvement, we raise awareness and empower people to help protect these incredible creatures. </p>
 
+                <div className='info-container'>
+                    <p className='info-text'>At The Heart of Down Red Squirrel Charity, we are dedicated to protecting and conserving one of the UK's most iconic and endangered species – the red squirrel.  </p>
+                        
+                    <p className='info-text'> Once widespread across the country, red squirrels are now under threat due to habitat loss, disease, and competition from the invasive grey squirrel.With fewer than 120,000 red squirrels left in the wild, their future is uncertain, but together, we can make a difference.   </p>
+
+                    <p className='info-text'>  Our charity focuses on vital conservation efforts such as habitat restoration, the installation of squirrel boxes, and monitoring squirrel populations to ensure their survival. Through education and community involvement, we raise awareness and empower people to help protect these incredible creatures. </p>
+                </div>
             <div className='box-container'>
                 <div className='box-item'>
                     <div className='box-item-1' id="box-1"></div>
